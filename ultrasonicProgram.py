64fe8477b564792ffc7cabcb1ce9a7e726410d34
jrajasekera <juderajasekera@gmail.com>
2018-10-07 12:50:47 +0000

    Change what prints to terminal

--- ultrasonicProgram.py
+++ ultrasonicProgram.py
@@ -20,28 +20,34 @@ s90 = DistanceSensor(echo=24, trigger=25)
 s135 = DistanceSensor(echo=19, trigger=26)
 s180 = DistanceSensor(echo=18, trigger=23)
 
-#initialize threshold values
+#initialize threshold and other key values
 s0_s180_Thresh = .5
 s45_s135_Thresh = .7
 s90_Thresh = .7
 refreshRate = .1
+speakPauseLength = .8
 
 # detect objects
 while True:
    print("s180 = " + toString(s180.distance) +"     s135 = " + toString(s135.distance) + "     s90 = " + toString(s90.distance) +"     s45 = "+ toString(s45.distance) + "     s0 = " + toString(s0.distance))
    time.sleep(refreshRate)
    if (s90.distance < s90_Thresh):
+         print("FRONT")
          speakIt("Stop! Object in front")
-         time.sleep(1)
+         time.sleep(speakPauseLength)
    elif(s45.distance < s45_s135_Thresh):
+         print("SLIGHT RIGHT")
          speakIt("Object slight right")
-         time.sleep(1)
+         time.sleep(speakPauseLength)
    elif(s135.distance < s45_s135_Thresh):
+         print("SLIGHT LEFT")
          speakIt("Object slight left")
-         time.sleep(1)
+         time.sleep(speakPauseLength)
    elif(s0.distance < s0_s180_Thresh):
+         print("RIGHT")
          speakIt("Object on right")
-         time.sleep(1)
+         time.sleep(speakPauseLength)
    elif(s180.distance < s0_s180_Thresh):
+         print("LEFT")
          speakIt("Object on left")
-         time.sleep(1)
+         time.sleep(speakPauseLength)
